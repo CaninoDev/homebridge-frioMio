801e452aee891f6e58fa5e7330ebbb6c56a7a784
Canino Developments <ian.canino@gmail.com>
2018-07-29 18:13:29 -0400

    implemented class constructor

--- index.js
+++ index.js
@@ -7,3 +7,29 @@ module.exports = function (homebridge) {
   Characteristic = homebridge.hap.Characteristic
   homebridge.registerAccessory('homebridge-lirc-LG-aircon', 'LGaircon', ACaccessory)
 }
+/* Platform constructor,
+ * config may be null.
+ */
+class LGaircon {
+  constructor (log, config) {
+    /* General Attributes */
+    this.name = config.name
+    this.serialNumber = config.serial
+    this.model = config.model
+    this.manufacturer = config.manufacturer
+    this.log = log
+    this.service = []
+
+    /* Initial States */
+    this.powerState = 0
+    this.mode = 'cool'
+    this.acTemperatureSetting = 60
+    this.temperatureDisplayUnits = config.temperatureUnit.toUpperCase() === 'CELSIUS' ? Characteristic.TemperatureDisplayUnits.CELSIUS : Characteristic.TemperatureDisplayUnits.FAHRENHEIT
+
+    /* Bring it all together */
+    this.log(`Initializing ${this.name}...`)
+
+    this.addServices()
+    this.bindCharacteristics()
+  }
+}
